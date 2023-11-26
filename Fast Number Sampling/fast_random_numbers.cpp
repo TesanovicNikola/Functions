083afd7ac3ee757689e34b5a8e17b8a34710b74e
Nikola Tešanović <100740409+Engatsuo@users.noreply.github.com>
2023-11-26 02:07:58 +0100

    Formating

--- Fast Number Sampling/fast_random_numbers.cpp
+++ Fast Number Sampling/fast_random_numbers.cpp
@@ -3,11 +3,14 @@
 #include <random>
 
 // [[Rcpp::export]]
-std::vector<double> generateRandomNumbers(int size, Rcpp::Nullable<Rcpp::NumericVector> weights = R_NilValue,
+std::vector<double> generateRandomNumbers(int size, 
+                                          Rcpp::Nullable<Rcpp::NumericVector> weights = R_NilValue,
                                           double minVal = 0, double maxVal = 1) {
+  
   std::vector<double> result(size); // Allocate memory for 'size' elements
   
   if (!weights.isNull() && Rf_length(weights) > 0) {
+    
     Rcpp::NumericVector w(weights);
     std::vector<double> weightsVec(w.begin(), w.end());
     
@@ -19,7 +22,9 @@ std::vector<double> generateRandomNumbers(int size, Rcpp::Nullable<Rcpp::Numeric
       int randomIndex = static_cast<int>(gen() % weightsVec.size());
       result[i] = minVal + (maxVal - minVal) * weightsVec[randomIndex];
     }
+    
   } else {
+    
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_real_distribution<> distribution(minVal, maxVal);
@@ -27,6 +32,7 @@ std::vector<double> generateRandomNumbers(int size, Rcpp::Nullable<Rcpp::Numeric
     for (int i = 0; i < size; ++i) {
       result[i] = distribution(gen);
     }
+    
   }
   
   return result;
